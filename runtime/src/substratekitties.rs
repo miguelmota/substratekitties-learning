f9683fb6dd949b366907b5da8f228957bee5d114
Miguel Mota <miguelmota2@gmail.com>
2019-02-03 02:37:15 -0800

    storage for tracking kitties

--- runtime/src/substratekitties.rs
+++ runtime/src/substratekitties.rs
@@ -15,7 +15,6 @@ pub trait Trait: balances::Trait {
     type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
-
 decl_event!(
     pub enum Event<T>
     where
@@ -30,6 +29,12 @@ decl_storage! {
     trait Store for Module<T: Trait> as KittyStorage {
         Kitties get(kitty): map T::Hash => Kitty<T::Hash, T::Balance>;
         KittyOwner get(owner_of): map T::Hash => Option<T::AccountId>;
+
+        AllKittiesArray get(kitty_by_index): map u64 => T::Hash;
+        AllKittiesCount get(all_kitties_count): u64;
+        AllKittiesIndex: map T::Hash => u64;
+
+
         OwnedKitty get(kitty_of_owner): map T::AccountId => T::Hash;
 
         Nonce: u64;
@@ -41,9 +46,12 @@ decl_module! {
 
         fn deposit_event<T>() = default;
 
-
         fn create_kitty(origin) -> Result {
             let sender = ensure_signed(origin)?;
+
+            let all_kitties_count = Self::all_kitties_count();
+
+            let new_all_kitties_count = all_kitties_count.checked_add(1).ok_or("Overflow adding a new kitty to total supply")?;
 
             let nonce = <Nonce<T>>::get();
             let random_hash = (<system::Module<T>>::random_seed(), &sender, nonce)
@@ -60,6 +68,12 @@ decl_module! {
 
             <Kitties<T>>::insert(random_hash, new_kitty);
             <KittyOwner<T>>::insert(random_hash, &sender);
+
+            <AllKittiesArray<T>>::insert(all_kitties_count, random_hash);
+            <AllKittiesCount<T>>::put(new_all_kitties_count);
+            <AllKittiesIndex<T>>::insert(random_hash, all_kitties_count);
+
+
             <OwnedKitty<T>>::insert(&sender, random_hash);
 
             <Nonce<T>>::mutate(|n| *n += 1);
